c04f2663e48aab9dbf5b722285b420d2cba47462
finetype <finesttype@gmail.com>
2015-09-29 00:11:24 -0500

    fixed nasty bug due to parseInt ommision, added some comments, lots of code cleanup

--- custom.js
+++ custom.js
@@ -18,36 +18,38 @@ var buttonActions = {
   months: life.years.months,
   weeks: life.years.months.weeks,
   days: life.years.months.weeks.days*/
-}
+};
 
 function drawLife(){
   scale = $( 'input.timeScale:checked' ).val();
   optimism = $('#optimism')[0].value;
-  debugger;
-  // birthday.year = $('#birthday').val().split("/")[2].slice(0,4)
-  currentAge = (now.format("YYYY") - birthday.year)
-  console.log(currentAge)
+  birthday.year = parseInt($('#birthday').val().split("/")[2].slice(0,4));
+  currentAge = (now.format("YYYY") - birthday.year);
+  console.log(currentAge);
   timeLeft = 90 * optimism/100;
-  console.log("time left: " + timeLeft)
+  console.log("time left: " + timeLeft);
   percentLived = (100/((timeLeft + currentAge) / currentAge)).toFixed(2);
 
-  window.life = {years:[]}
+  console.log(birthday.year + currentAge + timeLeft);
+  window.life = {years:[]};
   for (var i = birthday.year; i < (birthday.year + currentAge + timeLeft); i++){
-    console.log("i")
-    window.life.years.push([i,1,2,3,4,5,6,7,8,9,10,11,12])
+    console.log(i);
+    window.life.years.push([i,1,2,3,4,5,6,7,8,9,10,11,12]);
   }
 
-  console.log(buttonActions[scale])
+  console.log(buttonActions[scale]);
 
+  //these are coordinates for the squares
   var spot = [0,15];
 
-  $('.svgContainer').html("")
+  //this wipes the slate clean before every animation
+  $('.svgContainer').html("");
 
   var box = d3.select(".svgContainer")
    .append("svg")
    .attr("id","theCanvas")
    .attr("width",800)
-   .attr("height",600)
+   .attr("height",600);
    // .style("border", "1px solid black");
 
   var circs = box
@@ -56,12 +58,12 @@ function drawLife(){
     .enter()
     .append("rect")
     .attr("x", function(year,i){
-      spot[0] = (i%10 === 0 ? 168 : spot[0] + 50)
-      return spot[0]
+      spot[0] = (i%10 === 0 ? 168 : spot[0] + 50);
+      return spot[0];
     })
     .attr("y", function(year,i){
-      if (i % 10 === 0 && i !== 0) spot[1]+=50
-      return spot[1]
+      if (i % 10 === 0 && i !== 0) spot[1]+=50;
+      return spot[1];
     })
     .attr("width", 35)
     .attr("height", 35)
@@ -74,7 +76,7 @@ function drawLife(){
     .style("fill-opacity","1")
     .style("fill",function(d){
       return (d[0] - currentAge) > birthday.year ? "orange" : "grey";
-    })
+    });
 
   // var overlay = circs.append("text").text(function (d) {
   //     return d[0]-birthday.year;
@@ -87,8 +89,12 @@ function drawLife(){
   //   })
   //   .attr('text-anchor', 'middle')
   //   .style("fill", "white").style("stroke-width", 1.5);
-  $('rect').on('click', function(){ $(this).css('fill', 'rgb(128, 200, 128)') })
-  $('.results').html("Years left of life: <strong>" + timeLeft + "</strong><br/>Presumed total length of life: <strong>" 
-    +  (currentAge + timeLeft) + " years</strong><br/>Percent of life lived: <strong>" + percentLived + "%</strong>"
-    + "<br/> Year of Death: <strong>" + (Math.ceil(parseInt(now.format("YYYY")) + timeLeft)) + "</strong>")
+
+  $('rect').on('click', function(){ $(this).css('fill', 'rgb(128, 200, 128)'); });
+  $('.results').html(
+    "Years left of life: <strong>" + timeLeft +
+    "</strong><br/>Presumed total length of life: <strong>" + (currentAge + timeLeft) +
+    " years</strong><br/>Percent of life lived: <strong>" + percentLived +
+    "%</strong>" + "<br/> Year of Death: <strong>" + (Math.ceil(parseInt(now.format("YYYY")) + timeLeft)) + "</strong>"
+  );
 }
